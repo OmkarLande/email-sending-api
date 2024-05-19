414fddadad1fb1382f54a0f518cd31efca3e558e
Og_Om <84120403+OmkarLande@users.noreply.github.com>
2024-05-19 16:33:58 +0530

    ```text feat: Update MongoDB connection URL to use environment variable
    This commit updates the MongoDB connection URL in the `index.js` file to use the `DB_URL` environment variable if it is available. This allows for more flexibility in configuring the database connection.

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use('/lists', listsRouter);
 app.use('/users', usersRouter); 
 
-mongoose.connect('mongodb://localhost:27017/user-list-management', {
+mongoose.connect((process.env.DB_URL || 'mongodb://localhost:27017/user-list-management'), {
   useNewUrlParser: true,
   useUnifiedTopology: true
 }).then(() => {
